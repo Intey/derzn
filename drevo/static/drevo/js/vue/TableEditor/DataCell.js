7350b0dcdbcb707a4efb7bc2cd2e9784897efab0
RomanBorovskiy <borovskiyroman@gmail.com>
2025-02-07 14:47:02 +0300

    Добавлен обработчик на двойной клик - вызывается редактирование ячейки, если это текст

--- drevo/static/drevo/js/vue/TableEditor/DataCell.js
+++ drevo/static/drevo/js/vue/TableEditor/DataCell.js
@@ -1,11 +1,17 @@
 import {store} from './store.js'
-
+const { ElMessage } = ElementPlus
 export default {
     props: ['rowId','colId'],
 
     data(){
-        return { store }
+        return {
+        store,
+        timer: 0,
+         }
     },
+   mounted(){
+    //console.log('i mounted')
+   },
    computed: {
     classObject() {
         let cell = store.tableData.getCell(this.rowId,this.colId)
@@ -17,9 +23,27 @@ export default {
   },
     methods: {
         onClick() {
-            store.selected.select_element('d', [this.rowId,this.colId])
-       }
+            store.selected.select_element('d', [this.rowId,this.colId]);
+            let now = new Date()
+            let period = now - this.timer/1
+            if (period <220) {
+                //dblclick
+                this.onEdit()
+            }
+            else this.timer = now
+       },
+       onEdit() {
+       const [rowId, colId ] = store.selected.elementId
+        const cell = store.tableData.getCell(rowId, colId)
+        if (cell.id) {
+            ElMessage.error('нельзя менять текст в ячейке со знанием')
+            return
+        }
+        this.$root.prompt(cell.text, (value) => {
+            if (value)  store.tableData.setCellText(rowId, colId, value)
+            })
 
+       },
     },
 template: `
 <td :class="classObject"
